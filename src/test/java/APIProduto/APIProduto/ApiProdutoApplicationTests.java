e13ff4d85cd7e63b5298a47c001129606664e5e1
Caique do Vale <caiquedovale_@hotmail.com>
2022-11-21 19:13:33 -0300

    ProvaFinal - Desenvolvimento

---
+++ src/test/java/APIProduto/APIProduto/ApiProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package APIProduto.APIProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
